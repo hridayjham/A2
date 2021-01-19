85faf9f584e3e9490b91c0aef8b953298ddfe44d
hridayjham <jhamh@mcmaster.ca>
2021-01-19 00:49:41 -0500

    First test Commit

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -3,3 +3,19 @@
 #  @brief 
 #  @date 
 
+class ComplexT: 
+    x = 0.0
+    y = 0.0
+      
+    # parameterized constructor 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y
+
+    def real(self):
+        return self.x
+
+    def imaf(self):
+        return self.y
+
+     
