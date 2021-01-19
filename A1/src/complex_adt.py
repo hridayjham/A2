c7a5cd60008340d7fc8f57b29aa0f85732874d3d
hridayjham <jhamh@mcmaster.ca>
2021-01-19 15:17:00 -0500

    Completed Step 1

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -45,8 +45,35 @@ class ComplexT:
     def sub(self, ComplexNo):
         return ComplexT(self.x - ComplexNo.x, self.y - ComplexNo.y)
 
-a = ComplexT(5.0, 5.0)
-b = ComplexT(1.0, 1.0)
-c = a.sub(b)
-print(c.real())
-print(c.imag())
+    def mult(self, ComplexNo):
+        return ComplexT((self.x * ComplexNo.x - self.y * ComplexNo.y), (self.x * ComplexNo.y + self.y * ComplexNo.x)) 
+
+    def recip(self):
+        if self.x == 0 and self.y == 0:
+            return None
+        else:
+            realPart = self.x / (math.pow(self.x, 2) + math.pow(self.y, 2))
+            imagPart = -1 * self. y / (math.pow(self.x, 2) + math.pow(self.y, 2))
+            return ComplexT(realPart, imagPart)
+
+    def div(self, ComplexNo):
+        return self.mult(ComplexNo.recip())
+    #def div(self, ComplexNo):
+        
+    def sqrt(self):
+        sign = 0.0
+        if self.y < 0:
+            sign = -1.0
+        elif self.y > 0:
+            sign = 1.0
+        else:
+            sign = 0.0
+        realPart = math.sqrt((self.x + self.get_r()) / 2)
+        imagPart = sign * math.sqrt(( -self.x + self.get_r()) / 2)
+        return ComplexT(realPart, imagPart)
+
+##a = ComplexT(3.0, 3.0)
+##b = ComplexT(1.0, -1.0)
+##c = a.sqrt()
+##print(c.real())
+##print(c.imag())
