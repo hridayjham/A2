c01f46c9e7fe29a6a99b94f41b674fcd55fe0954
hridayjham <jhamh@mcmaster.ca>
2021-02-14 06:05:23 -0500

    Implemented Scene but it still has some issues to be resolved

--- A2/src/Scene.py
+++ A2/src/Scene.py
@@ -3,3 +3,54 @@
 #  @brief
 #  @date
 #  @details
+
+
+from Shape import Shape
+from scipy import *
+
+
+class Scene:
+    
+    def __init__(self, s, Fx, Fy, vx, vy):
+        self.s = s
+        self.Fx = Fx
+        self.Fy = Fy
+        self.vx = vx
+        self.vy = vy
+        
+    def get_shape(self):
+        return self.s
+    
+    def get_unbal_forces(self):
+        return self.Fx, self.Fy
+    
+    def get_init_velo(self):
+        return self.vx, self.vy
+    
+    def set_shape(self, s):
+        self.s = s
+    
+    def set_unbal_forces(self, Fx, Fy):
+        self.Fx = Fx
+        self.Fy = Fy
+        
+    def set_init_velo(self, vx, vy):
+        self.vx = vx
+        self.vy = vy
+        
+        
+    def __ode(self, w, t):
+        t = [w[2], w[3], self.Fx(t)/self.mass(), self.Fy(t)/self.mass]   
+    
+    
+    def sim(self, tfinal, nsteps):
+        t = []
+        for i in range(nsteps):
+            t.append(i.tfinal/(nsteps - 1))
+            
+        return t, odeint(self.ode, self.cm_x(), )
+        
+    
+    
+        
+    
