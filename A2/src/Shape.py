9dfe463fadf922f2a5f6fcec1b0714ff1f349555
hridayjham <jhamh@mcmaster.ca>
2021-02-16 12:38:38 -0500

    made some styling changes

--- A2/src/Shape.py
+++ A2/src/Shape.py
@@ -6,23 +6,19 @@
 from abc import ABC, abstractmethod
 
 class Shape(ABC):
-    
+
     @abstractmethod
-    
     def cm_x(self):
         pass
 
     @abstractmethod
-    
     def cm_y(self):
         pass
-    
+
     @abstractmethod
-    
     def mass(self):
         pass
-    
+
     @abstractmethod
-    
     def m_inert(self):
         pass
