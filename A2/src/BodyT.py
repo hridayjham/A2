d57beded7f1415b92ebbd1949a5cac6892f9801f
hridayjham <jhamh@mcmaster.ca>
2021-02-16 12:37:31 -0500

    Made the local functions private

--- A2/src/BodyT.py
+++ A2/src/BodyT.py
@@ -6,40 +6,32 @@
 from Shape import Shape
 import math
 
-def cm(z, m):
-    temp = 0
-    for i in range(len(m)):
-        temp += z[i]*m[i]
-    return temp/sum(m)
-
-def mmom(x, y, m):
-    temp = 0
-    for i in range(len(m)):
-        temp += m[i]*(math.pow(x[i], 2)+ math.pow(y[i], 2))
-    return temp
 
 class BodyT(Shape):
     
     def __init__(self,x, y, m ):
         if ((len(x) == len(y) and len (y) == len(m)) == False):
             raise ValueError("Lengths of inputs are invalid")
-        elif sum(m) <= 0 :
-            raise ValueError("Invalid input for m")
         else:
-            self.cmx = cm(x, m)
-            self.cmy = cm(y, m)
+            for i in m:
+                if i <= 0:
+                    raise ValueError("m values cannot be 0 or negative")
+            self.cmx = self.__cm(x, m)
+            self.cmy = self.__cm(y, m)
             self.m = sum(m)
-            self.moment = mmom(x, y, m) - sum(m)*(math.pow(cm(x, m), 2) + math.pow(cm(y, m), 2))
+            self.moment = self.__mmom(x, y, m) - sum(m)*(math.pow(self.__cm(x, m), 2) + math.pow(self.__cm(y, m), 2))
             
-        
-
-
-    def __mmom(x, y, m):
+    def __cm(self, z, m):
+        temp = 0
+        for i in range(len(m)):
+            temp += z[i]*m[i]
+        return temp/sum(m)   
+    
+    def __mmom(self, x, y, m):
         temp = 0
         for i in range(len(m)):
             temp += m[i]*(math.pow(x[i], 2)+ math.pow(y[i], 2))
-        return temp
-
+        return temp 
             
     def cm_x(self):
         return self.cmx
