832cd8c480b7113321399f188869ed4c43cefcb0
Christopher Schankula <schankuc@mcmaster.ca>
2021-03-04 18:33:59 -0500

    add blank Midterm files - Best wishes! :)

---
+++ Midterm/src/SeqServicesLibrary.py
@@ -0,0 +1,6 @@
+## @file SeqServicesLibrary.py
+#  @author Your name
+#  @brief Library module that provides functions for working with sequences
+#  @details This library assumes that all functions will be provided with arguments of the expected types
+#  @date 03/04/2021
+
---
+++ Midterm/src/SetOfInt.py
@@ -0,0 +1,4 @@
+## @file SetOfInt.py
+#  @author Your Name
+#  @brief Set of integers
+#  @date 03/04/2021
