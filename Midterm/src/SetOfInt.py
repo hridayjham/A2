c7a89a7e85aa72dbfa740a8ecfd0fb88aa4f47d9
hridayjham <jhamh@mcmaster.ca>
2021-03-05 07:30:09 +0530

    Implemented the template module

--- Midterm/src/SetOfInt.py
+++ Midterm/src/SetOfInt.py
@@ -2,3 +2,48 @@
 #  @author Your Name
 #  @brief Set of integers
 #  @date 03/04/2021
+
+
+class SetOfInt:
+    
+    def __init__(self, xs):
+        set1 = []
+        for x in xs:
+            set1.append(x)
+        self.s = set(set1)
+            
+    def is_member(self, x):
+        return x in self.s
+    
+    def to_seq(self):
+        return list(self.s)
+    
+    def union(self, t):
+        self_list = self.to_seq()
+        t_list = t.to_seq()
+        for i in t_list:
+            self_list.append(i)
+        return SetOfInt(self_list)
+    
+    def diff(self, t):
+        self_list = self.to_seq()
+        t_list = t.to_seq()
+        new_list = []
+        for i in self_list:
+            if i in t_list == False:
+                new_list.append(i)
+        return SetOfInt(new_list)
+    
+    def size(self):
+        return len(self.s)
+    
+    def empty(self):
+        return self.size() <= 0
+    
+    def equals(self, t):
+        flag = True
+        for i in self.s:
+            if i in t.to_seq() == False:
+                flag = False
+        return flag
+        
